425533979202a344a8405c6fcebbcee3fff14be3
Kent Solis <kentsolis77@gmail.com>
2025-11-12 19:12:36 -0700

    this is my first commit for the python fibonnaci practice assignment

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,10 @@
 def fibonacci(n):
-  pass
-
-
+    #first I place my base cases, for if n <= 0 || n = 1
+   if n < 0:
+      return "Incorrect input"
+   elif n == 0:
+      return 0
+   elif n == 1:
+      return 1
+   else:
+      return fibonacci(n-1) + fibonacci(n-2)
